c314d3280b5097287c34c3f3e61d52d369812336
teacher <tibedox@mail.ru>
2024-11-09 17:18:42 +0600

    33 cows

--- core/src/main/java/ru/saturday/satcow/Main.java
+++ core/src/main/java/ru/saturday/satcow/Main.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -13,27 +14,23 @@ public class Main extends ApplicationAdapter {
     private Texture image;
     public static final float SCR_WIDTH = 1280;
     public static final float SCR_HEIGHT = 720;
-    Cow cow, cow1, cow2;
+    Cow[] cow = new Cow[33];
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("cow0.png");
-        cow = new Cow(0, 0, 100, 100);
-        cow1 = new Cow(500, 500, 200, 200);
-        cow2 = new Cow(800, 300, 150, 150);
+        for (int i=0; i<cow.length; i++)
+            cow[i] = new Cow(SCR_WIDTH/2, SCR_HEIGHT/2, MathUtils.random(100, 200), MathUtils.random(100, 200));
     }
 
     @Override
     public void render() {
-        cow.fly();
-        cow1.fly();
-        cow2.fly();
+        for (int i=0; i<cow.length; i++) cow[i].fly();
         ScreenUtils.clear(0.55f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, cow.x, cow.y, cow.width, cow.height);
-        batch.draw(image, cow1.x, cow1.y, cow1.width, cow1.height);
-        batch.draw(image, cow2.x, cow2.y, cow2.width, cow2.height);
+        for (int i=0; i<cow.length; i++)
+            batch.draw(image, cow[i].x, cow[i].y, cow[i].width, cow[i].height);
         batch.end();
     }
 
