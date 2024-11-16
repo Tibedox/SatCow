d6eaf1231af92d63b25449affa981f5cdc062e83
teacher <tibedox@mail.ru>
2024-11-16 17:52:48 +0600

    orthographic camera & touch

--- core/src/main/java/ru/saturday/satcow/Main.java
+++ core/src/main/java/ru/saturday/satcow/Main.java
@@ -3,9 +3,11 @@ package ru.saturday.satcow;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -14,6 +16,9 @@ public class Main extends ApplicationAdapter {
     public static final float SCR_HEIGHT = 720;
 
     private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Vector3 touch;
+
     private Texture imgCow;
     private Texture imgPig;
     private Sound sndPig;
@@ -28,6 +33,10 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+        touch = new Vector3();
+
         imgCow = new Texture("cow0.png");
         imgPig = new Texture("pig.png");
         sndCow = Gdx.audio.newSound(Gdx.files.internal("sound-cow.mp3"));
@@ -38,12 +47,21 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
+        // касания
+        if(Gdx.input.justTouched()){
+            touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touch);
+
+            System.out.println(touch.x+" "+touch.y);
+        }
+
         // события
         spawnAnimals();
         moveAnimals();
 
         // отрисовка
         ScreenUtils.clear(0.55f, 0.15f, 0.2f, 1f);
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         for (int i = 0; i< numberLiveCows; i++) {
             batch.draw(imgCow, cow[i].x, cow[i].y, cow[i].width, cow[i].height);
@@ -67,13 +85,11 @@ public class Main extends ApplicationAdapter {
             float w = MathUtils.random(50, 200);
             cow[numberLiveCows] = new Cow(SCR_WIDTH/2, SCR_HEIGHT/2, w, w);
             numberLiveCows++;
-            //sndCow.play();
         }
         if(time%30 == 0 && numberLivePigs < pig.length){
             float w = MathUtils.random(50, 100);
             pig[numberLivePigs] = new Pig(SCR_WIDTH/2, SCR_HEIGHT/2, w, w);
             numberLivePigs++;
-            //sndPig.play();
         }
     }
 
